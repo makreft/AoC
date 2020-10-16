97e06144ad58a81541fc7a16e4a7f1bc5a24dc85
Marco Kreft <marco.kreft@lionsracing.de>
2020-10-17 00:02:16 +0200

    solution for AoC day 1

--- adv1.cpp
+++ adv1.cpp
@@ -1,21 +1,80 @@
 // reading a text file
+#include <queue>
+#include <numeric>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <cmath>
+
+
+std::vector<int> readInputToVector(std::string fileName)
+{
+    std::fstream inFile;
+    inFile.open(fileName); //app is append which means it will put the text at the end
+
+    std::vector<int> inNumbers;
+    if (inFile.is_open())
+    {
+      std::string line;
+      while(std::getline(inFile, line))
+      {
+        if(line.length())
+        {
+          int inNumber = std::stoi(line);
+          inNumbers.push_back(inNumber);
+        }
+      }
+      inFile.close();
+    }
+    else
+    {
+      std::cout << "nada" << std::endl;
+    }
+    return inNumbers;
+}
+
+int calcFuelForMass(int mass)
+{
+      return (std::floor(mass/3) - 2);
+}
+
+void calcFuel(std::vector<int>& mass, std::vector<int>& fuel)
+{
+   std::queue<int> q;
+   for (int i=0; i < mass.size(); ++i)
+   {
+    q.push(mass[i]);
+   }
+   std::cout << "im here" << std::endl;
+// 142195
+while (!q.empty())
+  {
+    int current_fuel(calcFuelForMass(q.front()));
+    if ( current_fuel > 0)
+    {
+      fuel.push_back(current_fuel);
+      q.push(current_fuel);
+    }
+    q.pop();
+  }
+}
 
 int main () {
-    std::string line;
-    std::ifstream myfile ("adv_input.txt");
-    if (myfile.is_open())
-    {
-        while ( std::getline (myfile,line) )
-        {
-            std::cout << line << '\n';
-        }
-        myfile.close();
-    }
+    std::vector<int> mass;
+    std::vector<int> result;
 
-    else std::cout << "Unable to open file";
+    mass=readInputToVector("adv_input.txt");
+    calcFuel(mass, result);
+    int total = 0;
+    for (int i=0; i < result.size(); ++i) 
+    {
+      total += result[i];
+    }
+    
+
+
+    std::cout << "Das Ergebnis ist " << total << std::endl;
 
     return 0;
 }
