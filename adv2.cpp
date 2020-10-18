6648354f2d2446020ad20a859d509d71f7957730
Marco Kreft <marco.kreft@lionsracing.de>
2020-10-18 15:31:16 +0200

    little bit of formatting

--- adv2.cpp
+++ adv2.cpp
@@ -8,29 +8,30 @@
 #include <vector>
 #include <cmath>
 
-void getNumberFromString(std::string str, std::vector<int>& vect) {
-std::stringstream ss(str);
-    for (int i; ss >> i;) {
-        vect.push_back(i);    
-        if (ss.peek() == ',')
+void getNumberFromString( std::string str, std::vector<int>& vect ) {
+std::stringstream ss( str );
+    for( int i; ss >> i; )
+    {
+        vect.push_back( i );
+        if( ss.peek() == ',' )
             ss.ignore();
     }
 }
 
-std::vector<int> readInputToVector(std::string fileName)
+std::vector<int> readInputToVector( std::string fileName )
 {
     std::fstream inFile;
-    inFile.open(fileName); //app is append which means it will put the text at the end
+    inFile.open( fileName ); //app is append which means it will put the text at the end
 
     std::vector<int> inNumbers;
-    if (inFile.is_open())
+    if(inFile.is_open() )
     {
       std::string line;
-      while(std::getline(inFile, line))
+      while(std::getline( inFile, line ) )
       {
         if(line.length())
         {
-          getNumberFromString(line, inNumbers);
+          getNumberFromString( line, inNumbers );
         }
       }
       inFile.close();
@@ -42,49 +43,49 @@ std::vector<int> readInputToVector(std::string fileName)
     return inNumbers;
 }
 
-int calcFuelForMass(int mass)
+int calcFuelForMass( int mass )
 {
-    return (std::floor(mass/3) - 2);
+    return( std::floor( mass / 3 ) - 2 );
 }
 
-void calcFuel(std::vector<int>& mass, std::vector<int>& fuel)
+void calcFuel( std::vector<int>& mass, std::vector<int>& fuel )
 {
   std::queue<int> q;
-  for (int i=0; i < mass.size(); ++i)
+  for( int i=0; i < mass.size(); ++i )
   {
-    q.push(mass[i]);
+    q.push( mass[i] );
   }
   std::cout << "im here" << std::endl;
 
-  while (!q.empty())
+  while( ! q.empty() )
   {
-    int current_fuel(calcFuelForMass(q.front()));
-    if ( current_fuel > 0)
+    int current_fuel( calcFuelForMass( q.front() ) );
+    if( current_fuel > 0 )
     {
-      fuel.push_back(current_fuel);
-      q.push(current_fuel);
+      fuel.push_back( current_fuel );
+      q.push( current_fuel );
     }
     q.pop();
   }
 }
 
-std::vector <std::vector<int> > inputToIntcodeFormat(std::vector<int> input)
+std::vector <std::vector<int> > inputToIntcodeFormat( std::vector<int> input )
 {
   std::vector <std::vector<int> > formatted;
   std::vector<int> intcode;
 
   int op_len=0;
 
-  for (int i=0; i < input.size(); ++i)
+  for( int i=0; i < input.size(); ++i )
   {
-    if (op_len < 4)
+    if( op_len < 4 )
     {
-      intcode.push_back(input[i]);
+      intcode.push_back( input[i] );
       op_len++;
     }
     else
     {
-      formatted.push_back(intcode);
+      formatted.push_back( intcode );
       op_len=0;
       intcode.clear();
     }
@@ -92,7 +93,7 @@ std::vector <std::vector<int> > inputToIntcodeFormat(std::vector<int> input)
   return formatted;
 }
 
-void add(int index, std::vector<int>& input)
+void add( int index, std::vector<int>& input )
 {
   // intcode = [opcode, position of val1, position of val2, store result to position]
   int pos1 = input[index+1];
@@ -100,7 +101,7 @@ void add(int index, std::vector<int>& input)
   int result = input[pos1] + input[pos2];
   input[input[index + 3]] = result;
 }
-void multiply(int index, std::vector<int>& input)
+void multiply( int index, std::vector<int>& input )
 {
   // intcode = [opcode, position of val1, position of val2, store result to position]
   int pos1 = input[index+1];
@@ -108,20 +109,20 @@ void multiply(int index, std::vector<int>& input)
   int result = input[pos1] * input[pos2];
   input[input[index + 3]] = result;
 }
-void calcIntcode(std::vector<int>&memory)
+void calcIntcode( std::vector<int>&memory )
 {
-  for (int i=0; i<memory.size(); i+=4)
+  for( int i=0; i<memory.size(); i+=4 )
   {
     int opcode = memory[i];
-    if (opcode == 1)
+    if(opcode == 1)
     {
-      add(i, memory);
+      add( i, memory );
     }
-    else if (opcode == 2)
+    else if( opcode == 2 )
     {
-      multiply(i, memory);
+      multiply( i, memory );
     }
-    else if ( opcode == 99 )
+    else if( opcode == 99 )
     {
       break;
     }
@@ -133,23 +134,24 @@ void calcIntcode(std::vector<int>&memory)
   }
 }
 
-int main () {
-  std::string file_name("adv2_input.txt");
+int main () 
+{
+  std::string file_name( "adv2_input.txt" );
   std::vector<int> input; // input = initial memory of intcode computer
 
-  input = readInputToVector(file_name);
+  input = readInputToVector( file_name );
   int instruction_ptr = 0;
 
-  std::vector<int> memory(input);
-  for (int noun=0; noun < 100; ++noun)
+  std::vector<int> memory( input );
+  for( int noun=0; noun < 100; ++noun )
   {
-    for (int verb=0; verb < 100; ++verb)
+    for( int verb=0; verb < 100; ++verb )
     {
       memory = input;
       memory[1] = noun;
       memory[2] = verb;
-      calcIntcode(memory);
-      if ( memory[0] == 19690720 )
+      calcIntcode( memory );
+      if( memory[0] == 19690720 )
       {
         std::cout << "Noun: " << noun << " Verb: " << verb << std::endl;
         return 0;
